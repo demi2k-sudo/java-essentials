808dc2873bf37ac098f22b7d3568668814d9cdc3
demi2k-sudo <85375873+demi2k-sudo@users.noreply.github.com>
2024-08-12 19:54:09 +0530

    peak

---
+++ 6-binary-search/src/Peak.java
@@ -0,0 +1,23 @@
+class Peak {
+    public static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid]<nums[mid+1]){
+                start = mid+1;
+            }
+            if(nums[mid]>nums[mid+1]){
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,5,4,3,2};
+        System.out.println(findPeakElement(arr));
+    }
+}
